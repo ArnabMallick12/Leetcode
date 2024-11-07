8d638c1f64093680e9ed4c58e7930a4b0e01f2c8
ArnabMallick12 <153369850+ArnabMallick12@users.noreply.github.com>
2024-11-08 00:36:02 +0530

    Time: 1048 ms (6.02%), Space: 61.7 MB (31.65%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0,j=height.size()-1;
+        int ans=0;
+        while(j>i){
+            int mh = min(height[i],height[j]);
+            int temp = (j-i)*mh;
+            if(ans<temp){
+                ans=temp;
+            }
+            cout<<ans<<" ";
+            if(height[i]>height[j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return ans;
+    }
+};
