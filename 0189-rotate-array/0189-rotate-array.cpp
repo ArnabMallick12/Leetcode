2388f415d1cadb9f418f10a1a7779dc2d809579d
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2025-01-15 19:39:45 +0530

    Time: 0 ms (100%), Space: 30.8 MB (5.18%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,13 +4,8 @@ public:
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
             ans.push_back(nums[(i+(nums.size()-((k+1)%nums.size()))+1)%nums.size()]);
-            cout<<(i+k+1)%nums.size()<<" "<<endl;
-            // cout<<ans[i-(k+1)]<<" ";
+            // cout<<(i+k+1)%nums.size()<<" "<<endl;
         }
         nums=ans;
-        for(int i=0;i<nums.size();i++){
-            cout<<ans[i]<<" ";
-            
-        }
     }
 };
