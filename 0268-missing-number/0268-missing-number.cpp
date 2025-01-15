e620fe5eb5db7f02ca70c09aa418730661c7ff4c
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2025-01-15 20:10:00 +0530

    Time: 0 ms (100%), Space: 21.7 MB (59.62%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        map<int,int> mp;
-        for(int i=0;i<nums.size()+1;i++){
-            mp[i]++;
-        }
+        int n = nums.size();
+        int nsum = (n*(n+1))/2;
+        int sum =0;
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+            sum+=nums[i];
         }
-        for(auto i :mp){
-            // cout<<i.first<<"-"<<i.second<<" ";
-            if(i.second == 1){
-                return i.first;
-            }
-        }
-        return 0;
+        return nsum-sum;
     }
 };
