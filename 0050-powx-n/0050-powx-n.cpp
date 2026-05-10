c1f8d6db691dd5455550b6241288c9ac9a111ff8
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-05-10 23:30:33 +0530

    Time: 0 ms (100%), Space: 8.8 MB (7.37%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        long long N = n;      
-        if (N < 0) {
-            x = 1.0 / x;      
-            N = -N;
+    double myPow(double x,int n) {
+        long long N=n;
+        if(N<0) {
+            x=1/x;
+            N=-1LL*N;
+        }
+        if(N==0) return 1;
+
+
+        double ans=1;
+        long long msb=log2(N);
+
+        for(int i=msb;i>=0;i--) {
+            ans=ans*ans;
+            if((N>>i)&1) {
+                ans=ans*x;
+            }
         }
 
-        double ans = 1.0;
-        while (N > 0) {
-            if (N & 1) {      
-                ans *= x;
-            }
-            x *= x;           
-            N >>= 1;          
-        }
         return ans;
     }
 };
