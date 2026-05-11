6d083a998a14466773693197e0471b4ea996b093
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-05-11 13:36:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            int req = target - nums[i];
+            auto it = mp.find(req);
+            if(it!=mp.end()){
+                return {i,it->second};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
