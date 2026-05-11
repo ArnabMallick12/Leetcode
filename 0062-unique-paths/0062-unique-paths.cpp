6adca5ddadda33f189b8d4f73d5200d51e16ef9c
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-05-11 13:28:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (87.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int sum = m+n-2;
-        int t = min(m-1,n-1);
-        long long ans=1;
-        for(int i=0;i<t;i++){
-            ans *=sum-i;
-            ans/=i+1;
+        int paths = (m-1)+(n-1);
+        long long ans = 1;
+        int steps = min(m-1,n-1);
+        for(int i=1;i<=steps;i++){
+            ans*=paths;
+            ans/=i;
+            paths--;
         }
         return ans;
     }
