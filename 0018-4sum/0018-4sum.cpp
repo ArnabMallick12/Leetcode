e2d9848e9164b4f672f1a50d91d1afb539aef409
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-05-11 13:49:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -5,13 +5,15 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) i++;
+            if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
                 if(j>i+1 && nums[j]==nums[j-1]) continue;
                 int k = j+1;
                 int l = n-1;
                 while(k<l){
-                    int sum = nums[i]+nums[j]+nums[k]+nums[l];
+                    long long sum = nums[i]+nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
                     if(sum==target){
                         ans.push_back({nums[i],nums[j],nums[k],nums[l]});
                         k++;
