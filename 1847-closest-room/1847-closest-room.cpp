dc10caf9682b38637b476d150e79dd7477e7f012
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-05-12 23:11:17 +0530

    Time: 1086 ms (11.88%), Space: 179.1 MB (89.11%) - LeetHub

--- 1847-closest-room/1847-closest-room.cpp
+++ 1847-closest-room/1847-closest-room.cpp
@@ -1,62 +1,55 @@
 class Solution {
 public:
+    int bs(int pref,int msize,vector<vector<int>>& arr){
+        int l=0,h=arr.size()-1;
+        int idx=-1;
 
-    int bs(int pref, int msize, vector<vector<int>>& arr) {
+        while(l<=h){
+            int m=(l+h)/2;
 
-        int l = 0, h = arr.size() - 1;
-        int idx = -1;
-
-        // first index with size >= msize
-        while (l <= h) {
-
-            int m = (l + h) / 2;
-
-            if (arr[m][1] >= msize) {
-                idx = m;
-                h = m - 1;
+            if(arr[m][1]>=msize){
+                idx=m;
+                h=m-1;
             }
-            else {
-                l = m + 1;
+            else{
+                l=m+1;
             }
         }
 
-        if (idx == -1) return -1;
+        if(idx==-1) return -1;
 
-        int ans = -1;
-        int minabs = INT_MAX;
+        int ans=-1;
+        int minabs=INT_MAX;
 
-        for (int i = idx; i < arr.size(); i++) {
+        for(int i=idx;i<arr.size();i++){
+            int diff=abs(arr[i][0]-pref);
 
-            int diff = abs(arr[i][0] - pref);
-
-            if (diff < minabs) {
-                minabs = diff;
-                ans = arr[i][0];
+            if(diff<minabs){
+                minabs=diff;
+                ans=arr[i][0];
             }
-            else if (diff == minabs) {
-                ans = min(ans, arr[i][0]);
+            else if(diff==minabs){
+                ans=min(ans,arr[i][0]);
             }
         }
 
         return ans;
     }
 
-    vector<int> closestRoom(vector<vector<int>>& rooms, vector<vector<int>>& queries) {
+    vector<int> closestRoom(vector<vector<int>>& rooms,vector<vector<int>>& queries){
 
-        sort(rooms.begin(), rooms.end(),
-            [](vector<int>& a, vector<int>& b) {
-
-                if (a[1] == b[1]) {
-                    return a[0] < b[0];
+        sort(rooms.begin(),rooms.end(),
+            [](vector<int>& a,vector<int>& b){
+                if(a[1]==b[1]){
+                    return a[0]<b[0];
                 }
-
-                return a[1] < b[1];
+                return a[1]<b[1];
             });
 
         vector<int> ans;
 
-        for (auto &q : queries) {
-            ans.push_back(bs(q[0], q[1], rooms));
+        for(auto q:queries){
+            ans.push_back(bs(q[0],q[1],rooms));
         }
 
         return ans;
