517b45002e66f77da7ca51355defa828b9ec32dc
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-06-13 13:35:48 +0530

    Time: 15 ms (51.84%), Space: 25.1 MB (31.71%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
-    int fn(int i,int target,vector<int>& nums){
-        if(i == nums.size()) {
-            if(target==0) return 1;
-            return 0;
+    int solve(int i, int sum, vector<int>& nums,
+              vector<vector<int>>& dp, int offset, int target) {
+
+        if (i == nums.size()) {
+            return (sum == target);
         }
 
-        int neg = fn(i+1,target - nums[i],nums);
-        int pos = fn(i+1,target+nums[i],nums);
-        return neg+pos;
+        if (dp[i][sum + offset] != -1)
+            return dp[i][sum + offset];
+
+        int add = solve(i + 1, sum + nums[i], nums,
+                        dp, offset, target);
+
+        int sub = solve(i + 1, sum - nums[i], nums,
+                        dp, offset, target);
+
+        return dp[i][sum + offset] = add + sub;
     }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        return fn(0,target,nums);
+        int total = accumulate(nums.begin(), nums.end(), 0);
+
+        if (abs(target) > total)
+            return 0;
+
+        int offset = total;
+
+        vector<vector<int>> dp(
+            nums.size(),
+            vector<int>(2 * total + 1, -1)
+        );
+
+        return solve(0, 0, nums, dp, offset, target);
     }
 };
