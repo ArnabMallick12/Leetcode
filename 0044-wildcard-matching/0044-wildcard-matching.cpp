454f0d729be78e369a7a3427d659f02127d664e6
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-06-19 21:21:08 +0530

    Time: 62 ms (30.5%), Space: 54.4 MB (22.58%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -37,8 +37,23 @@ public:
         int n = s.size();
         int m = p.size();
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        dp[0][0] =1;
+        for(int j=1;j<=m;j++){
+            if(isallstar(p, j-1)){
+                dp[0][j] =1;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?')
+                    dp[i][j] = dp[i-1][j-1];
 
-        return fn(n-1, m-1, s, p,dp);
+                if(p[j-1] == '*')
+                     dp[i][j] = dp[i-1][j] || dp[i][j-1];
+            }
+        }
+
+        return dp[n][m];
     }
 };
