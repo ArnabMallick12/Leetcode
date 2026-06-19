ec251eca1462f4e643101b5d0e33a9bb46f91795
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-06-19 19:55:16 +0530

    Time: 29 ms (59.39%), Space: 44.3 MB (32.46%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int fn(int i , int j,string s, string t,   vector<vector<int>>& dp){
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int pick =0;
+        if(s[i] == t[j]){
+            pick = fn(i-1,j-1,s,t,dp);
+            int npick = fn(i-1,j,s,t,dp);
+            return  dp[i][j] =  npick+pick;
+        }
+        else{
+            return dp[i][j] =  fn(i-1,j,s,t,dp);
+        }
+        
+
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size() , m= t.size();
+        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == t[j-1]){ 
+                    dp[i][j] =  dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j] =  dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
