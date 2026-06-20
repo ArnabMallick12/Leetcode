e2c0d8ba46f66f6069e4aa69e2998246b6f545d1
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-06-20 14:31:13 +0530

    Time: 0 ms (100%), Space: 20 MB (62.26%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -20,27 +20,33 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
-        dp[n][1]=dp[n][0] =0;
+        // vector<vector<int>> dp(n+1,vector<int>(2,0));
+        // dp[n][1]=dp[n][0] =0;
+        int notbuy =0 , buy = 0;
         for(int i = n-1;i>=0;i--){
             for(int j=0;j<2;j++){
                 int profit = 0;
                 if(j){
                     profit = max(
-                        -prices[i] + dp[i+1][0],
-                        dp[i+1][1]
+                        -prices[i] + notbuy,
+                        buy
                     );
+                    buy = profit;
                 }
                 else{
                     profit = max(
-                        +prices[i] + dp[i+1][1],
-                        dp[i+1][0]
+                        +prices[i] + buy,
+                        notbuy
                     );
+                    notbuy = profit;
                 }
-                dp[i][j] = profit;
+                // dp[i][j] = profit;
             }
         }
+        // for(int i = 0;i<=n;i++){
+        //     cout<<dp[i][0]<<" "<<dp[i][1]<<endl;
+        // }
 
-        return dp[0][1];
+        return buy;
     }
 };
