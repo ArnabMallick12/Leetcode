717739639c63561cd25910692d2dde3b9a8dada8
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-06-20 13:11:25 +0530

    Time: 0 ms (100%), Space: 97.5 MB (24.52%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int minele = prices[0];
-        int maxele = prices[0];
-        int maxprofit =0;
-        for(int i=1;i<prices.size();i++){
-            if (prices[i]<minele){
-                minele = prices[i];
-                maxele = prices[i];
-            }
-            if(prices[i]>maxele){
-                maxele = prices[i];
-            }
-            if((maxele - minele)>maxprofit){
-                maxprofit = maxele - minele;
-            }
+    int fn(int i,vector<int>& prices,vector<int>& dp){
+        if(i<0) return 0;
+        if(dp[i]!=-1 ) return dp[i];
+        int maxi = 0;
+        for(int j=i+1;j<prices.size();j++){
+            maxi = max(maxi, prices[j]-prices[i]);
         }
-        return maxprofit;
+        return dp[i] = max(maxi, fn(i-1,prices,dp));
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int minp  =  INT_MAX;
+        int mp  =0;
+        for(int i :prices){
+            minp = min(i,minp);
+            int p = i - minp;
+            mp = max(mp,p);
+        }
+        return mp;
     }
 };
