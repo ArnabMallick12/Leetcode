1f3c3cb8d3731f0eec157acd69741ab8878e6adc
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-06-22 23:57:54 +0530

    Time: 43 ms (73.61%), Space: 21.1 MB (12.84%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int fn(int i,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp){
-        if(j1<0 || j1>=grid[0].size() || j2<0 || j2>=grid[0].size()) return -1e9;
-        if(i == grid.size()-1) {
+    int fn(int i, int j1,int j2,vector<vector<int>>& grid ,vector<vector<vector<int>>>& dp){
+        if(i<0 || j1<0 || j2<0|| j1>=grid[0].size() || j2>=grid[0].size()) return 0;
+
+        if(i == grid.size()-1){
             if(j1==j2) return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
+            return grid[i][j1] + grid[i][j2];
         }
-        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-        int maxi = INT_MIN;
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        int maxi =0;
+        int val  = j1==j2 ? grid[i][j1] : grid[i][j1]+ grid[i][j2];
         for(int x=-1;x<=1;x++){
-            for(int y = -1;y<=1;y++){
-                maxi = max(maxi,fn(i+1,j1+x,j2+y,grid,dp));
+            for(int y =-1;y<=1;y++){
+                maxi = max(maxi,fn(i+1,j1+x,j2+y,grid,dp)+val);
             }
         }
-        if(j1==j2) return dp[i][j1][j2] =  grid[i][j1] + maxi;
-        else return dp[i][j1][j2] =  grid[i][j1] + grid[i][j2] + maxi;
+        return dp[i][j1][j2] = maxi;
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
-        int m  =grid[0].size();
+        int m = grid[0].size();
+
         vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
         return fn(0,0,m-1,grid,dp);
     }
