d3396faf49b7d1b1833f86aeea6c7f34937d7dfd
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-06-26 20:39:38 +0530

    Time: 11 ms (36.52%), Space: 19.6 MB (78.15%) - LeetHub

--- 3922-minimum-flips-to-make-binary-string-coherent/3922-minimum-flips-to-make-binary-string-coherent.cpp
+++ 3922-minimum-flips-to-make-binary-string-coherent/3922-minimum-flips-to-make-binary-string-coherent.cpp
@@ -12,21 +12,20 @@ public:
         // case 1 : all 1 or 0
         ans = min(totalOnes, n-totalOnes); // flip all 1->0 or 0->1 whichever is min
         //case 2 : ...000010000... exactly one 1
-        ans = min(ans, totalOnes-1); // only keep one 1 and flip rest of 1's
+        ans = min(ans, abs(totalOnes-1)); // only keep one 1 and flip rest of 1's
         //case 3: 10000....00001  1 at both the ends and 0's in between
         if(n>1){
             int cnt =0;
+            // if the ends are zero then flip it and count it
             if(s[0]=='0') cnt++;
             if(s[n-1]=='0') cnt++;
-             
+             // now add number of 1's in between to cnt
             for(int i=1;i<n-1;i++){
                 if(s[i]=='1') cnt++;
             }
             ans = min(cnt,ans);
-            // if()    // 1001100 , 001100 , 100111001
 
         }
-        if(ans<0) return 0;
         return ans;
     }
 };
