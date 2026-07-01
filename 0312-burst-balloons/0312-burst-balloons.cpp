206c03a023b95abcdd32812360c9f3d615f7d263
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-07-01 22:13:18 +0530

    Time: 99 ms (72.17%), Space: 13.8 MB (82.01%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -14,7 +14,17 @@ public:
         int n = nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return fn(1,n,nums,dp);
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        for(int i=n;i>=1;i--){
+            for(int j=i;j<=n;j++){
+                int maxi  =INT_MIN;
+                for(int k = i;k<=j;k++){
+                    int coins = nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    maxi = max(maxi,coins);
+                }
+                    dp[i][j] =  maxi;
+            }
+        }
+        return dp[1][n];
     }
 };
