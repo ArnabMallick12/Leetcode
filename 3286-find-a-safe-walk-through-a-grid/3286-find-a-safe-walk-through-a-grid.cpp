8e5f35073461c4196f071b1a42e7e837d2525e03
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-07-02 16:46:59 +0530

    Time: 18 ms (62.1%), Space: 30.2 MB (95.69%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    bool dfs(int r, int c, int health,
-             vector<vector<int>>& grid,
-             vector<vector<int>>& best) {
+    bool dfs(int r, int c, int health, vector<vector<int>>& grid, vector<vector<int>>& best) {
 
-        if (r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size())
-            return false;
-
+        if (r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size()) return false;
         health -= grid[r][c];
-
-        if (health <= 0)
-            return false;
-
-        if (r == grid.size() - 1 && c == grid[0].size() - 1)
-            return true;
-
-        if (best[r][c] >= health)
-            return false;
+        if (health <= 0) return false;
+        if (r == grid.size() - 1 && c == grid[0].size() - 1) return true;
+        if (best[r][c] >= health) return false;
 
         best[r][c] = health;
 
@@ -26,18 +16,14 @@ public:
         for (int i = 0; i < 4; i++) {
             int nr = r + dr[i];
             int nc = c + dc[i];
-
             if (dfs(nr, nc, health, grid, best))
                 return true;
         }
-
         return false;
     }
 
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        vector<vector<int>> best(grid.size(),
-                                 vector<int>(grid[0].size(), -1));
-
+        vector<vector<int>> best(grid.size(),vector<int>(grid[0].size(), -1));
         return dfs(0, 0, health, grid, best);
     }
 };
