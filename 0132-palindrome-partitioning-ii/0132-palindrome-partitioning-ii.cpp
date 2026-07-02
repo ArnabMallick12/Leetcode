d9376721faa7c9d4508c181c4b49ce13f8941da1
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-07-02 20:28:02 +0530

    Time: 99 ms (57.56%), Space: 33.4 MB (15.32%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(int l,int r ,string &s, vector<vector<int>> &pal){
         // int i=0, j =s.size()-1;
         int i =l, j = r;
-        if(pal[l][r]!=-1) return pal[i][j];
+        if(pal[l][r]!=-1) return pal[l][r];
         while(i<j){
             if(s[i]!=s[j]) return pal[l][r] =  false;
             i++;
@@ -26,14 +26,22 @@ public:
     //     return dp[i] = mini;
     // }
     int minCut(string s) {
+        int n = s.size();
         vector<int> dp(s.size()+1,0);
-        vector<vector<int>> pal(s.size(),vector<int>(s.size(),-1));
+        vector<vector<bool>> pal(n, vector<bool>(n, false));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s[i] == s[j] && (j - i < 2 || pal[i + 1][j - 1])) {
+                    pal[i][j] = true;
+                }
+            }
+        }
         for(int i = s.size()-1;i>=0;i--){
             int mini  = INT_MAX;
             string temp = "";
             for(int j=i;j<s.size();j++){
                 temp+= s[j];
-                if(isPalindrome(i,j,s,pal)){
+                if(pal[i][j]){
                     int cost = 1 + dp[j+1];
                     mini = min(mini,cost);
                 }
