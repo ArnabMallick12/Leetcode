34de4d38f61b3c625f86d88d31ec5574b02ffdf7
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-07-02 20:43:04 +0530

    Time: 7 ms (51.8%), Space: 12.6 MB (31.6%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fn(int i , int k ,vector<int>& arr,vector<int> &dp){
+        if(i == arr.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int maxAns = INT_MIN;
+        int maxi = INT_MIN;
+        int len =0;
+        for(int j =i;j< min((int)arr.size(),i+k);j++){
+            len++;
+            maxi = max(maxi,arr[j]);
+            int sum = len*maxi + fn(j+1,k,arr,dp);
+            maxAns = max(maxAns,sum);
+        }
+        return dp[i] = maxAns;
+    }
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp(n,-1);
+        return fn(0,k,arr,dp);
+    }
+};
