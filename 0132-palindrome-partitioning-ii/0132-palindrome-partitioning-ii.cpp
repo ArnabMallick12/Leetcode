75c813dea044d69fe3e3429bc24c4bdc1b21994f
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-07-02 20:18:49 +0530

    Time: 1631 ms (25.61%), Space: 84 MB (5.19%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isPalindrome(int i,int j ,string &s, vector<vector<int>> &pal){
+        // int i=0, j =s.size()-1;
+        if(pal[i][j]!=-1) return pal[i][j];
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
+        }
+        return pal[i][j] = true;
+    }
+    // int fn(int i ,string s ,vector<int> &dp){
+    //     if(i==s.size()) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int mini  = INT_MAX;
+    //     string temp = "";
+    //     for(int j=i;j<s.size();j++){
+    //         temp+= s[j];
+    //         if(isPalindrome(temp)){
+    //             int cost = 1 + fn(j+1,s,dp);
+    //             mini = min(mini,cost);
+    //         }
+    //     }
+    //     return dp[i] = mini;
+    // }
+    int minCut(string s) {
+        vector<int> dp(s.size()+1,0);
+        vector<vector<int>> pal(s.size(),vector<int>(s.size(),-1));
+        for(int i = s.size()-1;i>=0;i--){
+            int mini  = INT_MAX;
+            string temp = "";
+            for(int j=i;j<s.size();j++){
+                temp+= s[j];
+                if(isPalindrome(i,j,s,pal)){
+                    int cost = 1 + dp[j+1];
+                    mini = min(mini,cost);
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[0]-1;
+    }
+};
