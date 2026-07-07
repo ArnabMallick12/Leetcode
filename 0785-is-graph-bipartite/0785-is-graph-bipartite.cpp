8b0a00f66b6f3533c97b2346f1452cc49baa9cd1
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-07-08 02:52:04 +0530

    Time: 0 ms (100%), Space: 17.6 MB (39.46%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    bool fn(int node, int color, vector<int>& colors,vector<int>& vis,vector<vector<int>>& graph){
-        vis[node] = 1;
+    bool dfs(int node, int color, vector<int> &colors , vector<int> &vis, vector<vector<int>>& adj){
+        vis[node] =1;
         colors[node] = color;
-        cout<<node<<" color "<<color<<endl;
-        for(auto u : graph[node]){
-            if(vis[u]==-1){
-                if (!fn(u,!color,colors,vis,graph)) return false;
+        for(auto i : adj[node]){
+            if(!vis[i]){
+                if(!dfs(i,!color,colors,vis,adj)) return false;
             }
             else{
-                if(colors[u]==color) {
-                    cout<<node<<" "<<u<<"->false"<<endl;
-                    return false;
-                }
-                 cout<<node<<" "<<u<<"->true"<<endl;
+                if(colors[i] == color) return false;
             }
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n,-1);
+        vector<int> vis(n,0);
         vector<int> colors(n,-1);
-        int col=0;
         for(int i=0;i<n;i++){
-            if(vis[i]==-1){
-                    cout<<"main called node: "<<i<<endl;
-                if(fn(i,col,colors,vis,graph)==false) {
-                    return false;
-                }
-                // col=!col;
+            if(!vis[i]){
+                if(!dfs(i,0,colors,vis,graph)) return false;
             }
         }
         return true;
