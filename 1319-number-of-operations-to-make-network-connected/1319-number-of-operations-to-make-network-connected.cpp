5adbbb044fce7ab2713ca9117a361764463b6ed7
Arnab <153369850+ArnabMallick12@users.noreply.github.com>
2026-07-12 01:35:27 +0530

    Time: 19 ms (52.32%), Space: 48.9 MB (24.99%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,28 +1,70 @@
+class DisjointSet{
+    vector<int> rank;
+    vector<int> size;
+    public:
+    
+    vector<int> parent;
+    DisjointSet(int n){
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+            size[i] =1;
+        }
+        
+    }
+    
+    int find(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = find(parent[node]);
+    }
+    
+    void unionByRank(int u,int v){
+        int pu = find(u);
+        int pv = find(v);
+        if(pu==pv) return;
+        if(rank[pu]<rank[pv]){
+            parent[pu] = pv;
+        }
+        else if(rank[pv]<rank[pu]){
+            parent[pv] = pu;
+        }
+        else{
+            parent[pu] = pv;
+            rank[pv]++;
+        }
+        
+    }
+    void unionBySize(int u,int v){
+        int pu = find(u);
+        int pv = find(v);
+        if(pu==pv) return;
+        if(size[pu]<size[pv]){
+            parent[pu] = pv;
+            size[pv]+=size[pu];
+        }
+        else{
+            parent[pv] = pu;
+            size[pu]+=size[pv];
+        }
+        
+    }
+};
 class Solution {
 public:
-    void dfs(int node, vector<int>& vis,vector<vector<int>>& adj ){
-        vis[node] = 1;
-        for(auto u : adj[node]){
-            if(vis[u]==-1){
-                dfs(u,vis,adj);
-            }
-        }
-    }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(n-1>connections.size()) return -1;
-        vector<vector<int>> adj(n);
-        for(auto i : connections){
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+        DisjointSet ds(n);
+        int extraEdges = 0;
+        for(auto c : connections){
+            if(ds.find(c[0])==ds.find(c[1])) extraEdges++;
+            else ds.unionBySize(c[0],c[1]);
         }
-        int cnt =0;
-        vector<int> vis(n,-1);
+        unordered_set<int> components;
         for(int i=0;i<n;i++){
-            if(vis[i]==-1){
-                dfs(i,vis,adj);
-                cnt++;
-            }
+            components.insert(ds.find(i));
         }
-        return cnt-1;
+        if(components.size()-1 <= extraEdges) return components.size()-1;
+        return -1;
     }
 };
